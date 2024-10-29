48ae160d6e2d23dd5e53a39e3322a7c4b0eaf500
tehedor <sergiot1805@gmail.com>
2024-10-29 01:56:28 +0100

    new

--- pruebas/railroadSwitch/toggle_switch.py
+++ pruebas/railroadSwitch/toggle_switch.py
@@ -33,35 +33,62 @@
 ############################################################################################################
 ############################################################################################################
 ############################################################################################################
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 
+# GPIO.setmode(GPIO.BCM)
+
+# SWITCH_PIN = 6
+
+# DEBOUNCE_TIME_MS = 200  # 200 milliseconds
+
+# GPIO.setup(SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# switch_state = GPIO.input(SWITCH_PIN)
+# prev_switch_state = switch_state
+
+# def switch_callback(channel):
+#     global switch_state, prev_switch_state
+#     switch_state = GPIO.input(SWITCH_PIN)
+#     if switch_state != prev_switch_state:
+#         if switch_state == GPIO.HIGH:
+#             print("The switch: ON -> OFF")
+#         else:
+#             print("The switch: OFF -> ON")
+#         prev_switch_state = switch_state
+
+# GPIO.add_event_detect(SWITCH_PIN, GPIO.BOTH, callback=switch_callback, bouncetime=DEBOUNCE_TIME_MS)
+
+# try:
+#     # Main loop
+#     while True:
+#         pass  # Do other tasks here
+
+# except KeyboardInterrupt:
+#     GPIO.cleanup()
+
+############################################################################################################
+############################################################################################################
+import time
+import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
-SWITCH_PIN = 6
+btn_input = 6
+# LED_output = 17;
 
-DEBOUNCE_TIME_MS = 200  # 200 milliseconds
+# GPIO btn_input set up as input.
+GPIO.setup(btn_input, GPIO.IN)
+# GPIO.setup(LED_output, GPIO.OUT)
 
-GPIO.setup(SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# handle the button event
+def buttonEventHandler (pin):
+    # turn LED on/off
+    # GPIO.output(LED_output,True)
+    # time.sleep(5)
+    # GPIO.output(LED_output,False)
+    print("Button pressed")
 
-switch_state = GPIO.input(SWITCH_PIN)
-prev_switch_state = switch_state
-
-def switch_callback(channel):
-    global switch_state, prev_switch_state
-    switch_state = GPIO.input(SWITCH_PIN)
-    if switch_state != prev_switch_state:
-        if switch_state == GPIO.HIGH:
-            print("The switch: ON -> OFF")
-        else:
-            print("The switch: OFF -> ON")
-        prev_switch_state = switch_state
-
-GPIO.add_event_detect(SWITCH_PIN, GPIO.BOTH, callback=switch_callback, bouncetime=DEBOUNCE_TIME_MS)
-
-try:
-    # Main loop
-    while True:
-        pass  # Do other tasks here
-
-except KeyboardInterrupt:
-    GPIO.cleanup()
+GPIO.add_event_detect(btn_input, GPIO.RISING, callback=buttonEventHandler)  
+try:  
+    GPIO.wait_for_edge(btn_input, GPIO.FALLING)  
+except:
+    GPIO.cleanup()   
