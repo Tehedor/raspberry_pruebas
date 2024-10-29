ca27906af496aedc61a8ac2514b61de3d846ce20
tehedor <sergiot1805@gmail.com>
2024-10-29 01:38:03 +0100

    a

--- pruebas/railroadSwitch/toggle_switch.py
+++ pruebas/railroadSwitch/toggle_switch.py
@@ -71,7 +71,7 @@ def switch_callback(channel):
 
 # Add an event listener for the switch press
 # GPIO.add_event_detect(SWITCH_PIN, GPIO.BOTH, callback=switch_callback, bouncetime=DEBOUNCE_TIME_MS)
-GPIO.add_event_detect(SWITCH_PIN, GPIO.RISING, callback=switch_callback, bouncetime=DEBOUNCE_TIME_MS)
+GPIO.add_event_detect(SWITCH_PIN, callback=switch_callback, bouncetime=DEBOUNCE_TIME_MS)
 
 try:
     # Main loop
