f3adc7e8e2df1091668f6a3b143df9f96c049e50
tehedor <sergiot1805@gmail.com>
2024-10-29 01:29:16 +0100

    A

--- pruebas/railroadSwitch/toggle_switch.py
+++ pruebas/railroadSwitch/toggle_switch.py
@@ -1,11 +1,11 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-import time
+# import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+# import time
 
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+# GPIO.setwarnings(False) # Ignore warning for now
+# GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 
-# GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 26 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 26 to be an input pin and set initial value to be pulled low (off)
+# # GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 26 to be an input pin and set initial value to be pulled low (off)
+# GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 26 to be an input pin and set initial value to be pulled low (off)
 
 # import socket
 
