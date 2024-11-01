a7c153539868cfbb9ddb01767510ce2f745053ed
tehedor <sergiot1805@gmail.com>
2024-11-01 01:21:01 +0100

    configruar railroad switch en secirpt

---
+++ general/components/railroadSwitch.py
@@ -0,0 +1,70 @@
+from gpiozero import Button, AngularServo
+
+class RailroadSwitch:
+    def __init__(self, pin_switch, pin_servo):
+        self.switch = Button(pin_switch)  # define Button pin according to BCM Numbering
+        self.servo = AngularServo(pin_servo, initial_angle=0, min_angle=0, max_angle=180, min_pulse_width=0.5, max_pulse_width=2.5)
+        self.state = 0
+        
+    def control_switch(self):
+        if self.switch.is_pressed:  # if switch is pressed
+            if self.state == 0:
+                self.state = 1
+                print("State 1")
+                self.control_servo(self.state)
+        else:  # if switch is released
+            if self.state == 1:
+                self.state = 0
+                print("State 0")
+                self.control_servo(self.state)
+        
+    def control_servo(self, state):
+        if state == 0:
+            for angle in range(0, 91, 1):
+                self.servo.angle = angle
+        else:
+            for angle in range(90, -1, -1):
+                self.servo.angle = angle    
+    ########################################################                
+    ############## Broker                 
+    ########################################################                
+                
+    def broker_control_switch(self):
+        if self.switch.is_pressed:  # if switch is pressed
+            if self.state == 0:
+                self.state = 1
+                print("State 1")
+                self.send_request(self.state)
+        else:  # if switch is released
+            if self.state == 1:
+                self.state = 0
+                print("State 0")
+                self.send_request(self.state)                
+                
+    def broker_control_servo(self, state):
+        if state == 0:
+            for angle in range(0, 91, 1):
+                self.servo.angle = angle
+        else:
+            for angle in range(90, -1, -1):
+                self.servo.angle = angle    
+                
+    ########################################################                
+    ############## Request
+    ########################################################   
+    def send_request(self, state):
+        url = f"http://{self.ip_address}/update_state"
+        data = {'state': state}
+        try:
+            response = requests.post(url, json=data)
+            if response.status_code == 200:
+                print("Request successful")
+            else:
+                print(f"Request failed with status code: {response.status_code}")
+        except requests.exceptions.RequestException as e:
+            print(f"Request failed: {e}")
+        
+        
+        
+    def destroy(self):
+        self.switch.close()
--- general/script.py
+++ general/script.py
@@ -22,8 +22,8 @@ def main():
     enable_street_light = False
     enable_toll = False
     enable_crane = False
-    enable_weather_station = True
-    enable_railroad_switch = False
+    enable_weather_station = False
+    enable_railroad_switch = True
     enable_train = False
     enable_radar = False
     
@@ -33,13 +33,13 @@ def main():
     street_light = StreetLight(pir_led_pin=22, pir_sensor_pin=18, photo_led_pin=27, threshold=128) if enable_street_light else None
     toll = Toll(toll_pin=23) if enable_toll else None
     crane = Crane(pin_ultrasound_trig=16, pin_ultrasound_echo=26) if enable_crane else None
+    railroad_switch = RailroadSwitch(pin_switch=26, pin_servo=21) if enable_railroad_switch else None
     
     # weather_station = WeatherStation(pin_weatherSensor=11) if enable_weather_station else None   # Tienes que poner pin 11 auque por alguna razon corresponde al pin 17, sino no funciona 
     weather_station = WeatherStation(pin_weatherSensor=11, sleeptime=sleeptime) if enable_weather_station else None   # Tienes que poner pin 11 auque por alguna razon corresponde al pin 17, sino no funciona 
     
     train = Train() if enable_train else None
     radar = Radar() if enable_radar else None
-    railroad_switch = RailroadSwitch() if enable_railroad_switch else None
     
     # Crear una lista de funciones a ejecutar
     tasks = []
@@ -57,7 +57,7 @@ def main():
     if enable_radar:
         tasks.append(radar.some_method)  # Reemplaza some_method con el método adecuado
     if enable_railroad_switch:
-        tasks.append(railroad_switch.some_method)  # Reemplaza some_method con el método adecuado
+        tasks.append(railroad_switch.control_switch)  # Reemplaza some_method con el método adecuado
     
     # # Crear y lanzar un hilo para cada tarea
     # stop_event = threading.Event()
