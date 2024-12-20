cdfabfbaf97eaed56245a9f01bfc723577a0dae5
tehedor <sergiot1805@gmail.com>
2024-12-20 02:19:05 +0100

    dbg

--- general/components/radar.py
+++ general/components/radar.py
@@ -31,7 +31,7 @@ class Radar:
         picam2 = Picamera2()
         preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
         picam2.configure(preview_config)
-        picam2.start_preview(Preview.QTGL)
+        # picam2.start_preview(Preview.QTGL)
         picam2.start()
         time.sleep(0.5)
         timestamp = time.strftime("%Y%m%d_%H%M%S")
